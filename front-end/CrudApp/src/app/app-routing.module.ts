b856e7cc3b31423b310559d03342ced8ab2427b7
Reneilwe Sesoko <reneilwe.sesoko@dadevs.co.za>
2022-01-12 12:50:51 +0200

    Service, navbar and product landing

--- front-end/CrudApp/src/app/app-routing.module.ts
+++ front-end/CrudApp/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'products',pathMatch: 'full'},
+  {path: 'add', component: AddProductComponent },
+  {path: 'products', component: ProductDetailComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/CrudApp/src/app/services/product.service.ts
+++ front-end/CrudApp/src/app/services/product.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
+
+const baseUrl = "http://localhost:8080/api/tutorials";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //get all the data 
+  getAll(): Observable<Product[]>{
+    return this.http.get<Product[]>(baseUrl);
+  }
+
+  //get the data using id
+  getById(id: any): Observable<any>{
+    return this.http.get<Product[]>(baseUrl + '/' + id)
+  }
+
+  //create 
+  create(data: any): Observable<any>{
+    return this.http.post<Product[]>(baseUrl, data)
+  }
+
+  //update using id
+  update(id: any, data: any): Observable<any>{
+    return this.http.put<Product[]>(baseUrl + '/', data)
+  }
+
+  //delete by id
+  delete(id: any){
+    return this.http.delete(baseUrl + '/' + id)
+  }
+
+  //delete all
+  deleteAll(){
+    return this.http.delete(baseUrl)
+  }
+
+
 }
