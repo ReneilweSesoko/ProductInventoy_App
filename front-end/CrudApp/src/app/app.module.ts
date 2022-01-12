053b972ca8e6cbb6cb910da8edf5c34996d1d872
Reneilwe Sesoko <reneilwe.sesoko@dadevs.co.za>
2022-01-12 11:36:58 +0200

    added components, service and model files

--- front-end/CrudApp/src/app/app.module.ts
+++ front-end/CrudApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AddProductComponent,
+    ProductDetailComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
